cd49a2ea17b9962fee8cea99580647edaaa52a68
milek112 <mblotni@gmail.com>
2024-04-19 15:48:23 +0200

    500

--- nauczyciel.php
+++ nauczyciel.php
@@ -1,4 +1,13 @@
+<?php
+require_once('config/connect_dane.php');
+$query="SELECT * FROM dane_uczniow";
+$result = mysqli_query($conn, $query);
 
+if (!$result) {
+    die("Błąd zapytania SQL: " . mysqli_error($conn));
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pl-PL">
 <head>
@@ -22,8 +31,6 @@
             <li class="topnav-li"><a class="topnav-a" href="">Start<div class="underline"></div></a></li>
             <li class="topnav-li"><a class="topnav-a" href="">Jak to działa<div class="underline"></div></a></li>
             <li class="topnav-li"><a class="topnav-a" href="">Twoje zwolnienia<div class="underline"></div></a></li>
-
-
             <li style="position: relative;left:20%;background-color: blueviolet;border-radius: 20px;" class="topnav-li"><a class="topnav-a" href="sing-up.html">Utwórz konto<div class="underline"></div></a></li>
             <li style="position: relative;left:20%;background-color: rgb(212, 0, 255);border-radius: 20px;" class="topnav-li"><a class="topnav-a" href="zaloguj.html">Zaloguj się<div class="underline"></div></a></li>
         </ul>
@@ -57,3 +64,4 @@ while($row=mysqli_fetch_array($result))
     </div>
 </body>
 </html>
+
