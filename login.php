15e02bb02484199dd07338efcbb1c268c6a3dc18
KanonM3 <mateusz.kielbowicz86@gmail.com>
2024-04-19 14:20:51 +0200

    update

--- login.php
+++ login.php
@@ -7,19 +7,20 @@ if(isset($_POST['submit'])){
     $haslo=md5($haslo) ;
     
 
-    $sql="SELECT * FROM users WHERE email='$email' and password='$haslo'";
+    $sql="SELECT * FROM users WHERE email='$email' and haslo='$haslo'";
     $result=$conn->query($sql);
     if($result->num_rows>0){
        session_start();
        $row=$result->fetch_assoc();
        $_SESSION['email']=$row['email'];
        $uzytkownik = $row['uzytkownik'];
+       
        if($uzytkownik == 'Uczen') {
           header("Location: uczen.php");
        } elseif ($uzytkownik == 'Nauczyciel') {
-          header("Location: nauczyciel.php");
+          header("Location: widok_nauczyciela.php");
        } elseif ($uzytkownik == 'Lekarz') {
-          header("Location: lekarz.php");
+          header("Location: lekarz1.php");
        } else {
         echo "Error nieznany uzytkownik:".$conn->error;
        }
